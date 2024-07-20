ae37497d91a91489f32507f71c12d144e36c6b01
Rachael Oldham-Knott <reok@Rachaels-MacBook-Pro.local>
2024-07-20 20:45:33 +0100

    wrapped App with Chakra, styling to be added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react';
 import Quiz from "./Components/Quiz.jsx";
 import {Amplify} from 'aws-amplify';
 import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
@@ -8,28 +9,30 @@ Amplify.configure(awsExports);
 
 const App = () => {
     return (
-        <div className="App">
-            <Authenticator>
-                {({ signOut }) => (
-                    <main>
-                        <header className='App-header'>
-                            <Quiz />
-                            <button
-                                onClick={signOut}
-                                style={{
-                                    margin: '20px',
-                                    fontSize: '0.8rem',
-                                    padding: '5px 10px',
-                                    marginTop: '20px'
-                                }}
-                            >
-                                Sign Out
-                            </button>
-                        </header>
-                    </main>
-                )}
-            </Authenticator>
-        </div>
+        <ChakraProvider>
+            <div className="App">
+                <Authenticator>
+                    {({ signOut }) => (
+                        <main>
+                            <header className='App-header'>
+                                <Quiz />
+                                <button
+                                    onClick={signOut}
+                                    style={{
+                                        margin: '20px',
+                                        fontSize: '0.8rem',
+                                        padding: '5px 10px',
+                                        marginTop: '20px'
+                                    }}
+                                >
+                                    Sign Out
+                                </button>
+                            </header>
+                        </main>
+                    )}
+                </Authenticator>
+            </div>
+        </ChakraProvider>
     );
 }
 
