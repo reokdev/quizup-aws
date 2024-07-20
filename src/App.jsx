6f050842a2fc2350c0496718de482376d6d27df3
Rachael Oldham-Knott <reok@Rachaels-MacBook-Pro.local>
2024-07-20 11:27:33 +0100

    added App structure, ReactUI from Amplify

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import './App.css';
+import {Amplify} from 'aws-amplify';
+import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
+
 const App = () => {
     return (
-        <div>App</div>
-    )
+        <div className="App">
+            <Authenticator>
+                {({ Logout }) => (
+                    <main>
+                        <header className='App-header'>
+                            <button
+                                onClick={Logout}
+                                style={{
+                                    margin: '20px',
+                                    fontSize: '0.8rem',
+                                    padding: '5px 10px',
+                                    marginTop: '20px'
+                                }}
+                            >
+                                Logout
+                            </button>
+                        </header>
+                    </main>
+                )}
+            </Authenticator>
+        </div>
+    );
 }
 
-export default App;
+export default withAuthenticator(App);
